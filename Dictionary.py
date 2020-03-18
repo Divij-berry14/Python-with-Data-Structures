b5a29551f8d412bbd5c94c1f0c6036c6395769fe
Divij <divij.berry1999@gmail.com>
2020-03-18 23:36:43 +0530

    Dictionary For loop

--- Dictionary.py
+++ Dictionary.py
@@ -1,3 +1,9 @@
 a={1:2,3:4,"list":[1,23],"dict":{1:2}}
 print(a.get("list"))
-print(a.get(0))
+print(a.get("li",0)) # If the key is not present then it will return 0. Use of get will not show runtime error
+print(a.keys())
+print(a.values())
+print(a.items()) # it will get key:value pairs
+
+for i in a:
+    print(i,a[i])
