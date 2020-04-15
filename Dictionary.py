3bed5122791b145aeb1c65d57cf88b6a8240e2ee
Divij <divij.berry1999@gmail.com>
2020-04-15 23:57:49 +0530

    ;

--- Dictionary.py
+++ Dictionary.py
@@ -1,40 +1,66 @@
-a={1:2,3:4,"list":[1,23],"dict":{1:2}}
-print(a[1])
-print(a.get("list"))
-print(a.get("li", 0)) # If the key is not present then it will return 0. Use of get will not show runtime error
-print(a.keys())
-print(a.values())
-print(a.items()) # it will get key:value pairs
+# a={1:2,3:4,"list":[1,23],"dict":{1:2}}
+# print(a[1])
+# print(a.get("list"))
+# print(a.get("li", 0)) # If the key is not present then it will return 0. Use of get will not show runtime error
+# print(a.keys())
+# print(a.values())
+# print(a.items()) # it will get key:value pairs
+#
+# print("loop dic")
+# for i in a:
+#     print(i,a[i])
+#
+# for i in a.values():
+#     print(i)
+#
+# print("list" in a) # to check if the key exist or not. It will return TRUE
+#
+# a["t"]=(1,2,3,4)  # To add another key in the dictionary a
+# print(a)
+#
+# b={3:5,'the': 4,2 :100}
+# a.update(b)  # This function will update dictionary a on the basis of b dict
+# print(a) #{1: 2, 3: 5, 'list': [1, 23], 'dict': {1: 2}, 'tuple': (1, 2, 3, 4), 'the': 4, 2: 100}
+#
+# print(a.pop('t'))  #Pop fucntion requires 1 arguement
+# del a[1]
+# print(a)
+#
+# #Program to print words having frequency k
+# s= "This is a word string having many many word"
+# words=s.split()
+# d={}
+# for w in words:
+#     d[w]=d.get(w, 0)+1
+# print(d)
+# for w in d:
+#     if d[w] == 2:
+#         print(w)
 
-print("loop dic")
-for i in a:
-    print(i,a[i])
+def checkMagazine(magazine, note):
+    dm={}
+    dn={}
+    for w in magazine:
+        dm[w]=dm.get(w,0)+1
+    for w in note:
+        dn[w]=dn.get(w,0)+1
+    for w in dn:
+        if w in dm and dn[w]<=dm[w]:
+            flag=1
+        else:
+            flag=0
+            break
+    print(dm)
+    print(dn)
+    if flag==1:
+        print("Yes")
+    else:
+        print("No")
 
-for i in a.values():
-    print(i)
-
-print("list" in a) # to check if the key exist or not. It will return TRUE
-
-a["t"]=(1,2,3,4)  # To add another key in the dictionary a
-print(a)
-
-b={3:5,'the': 4,2 :100}
-a.update(b)  # This function will update dictionary a on the basis of b dict
-print(a) #{1: 2, 3: 5, 'list': [1, 23], 'dict': {1: 2}, 'tuple': (1, 2, 3, 4), 'the': 4, 2: 100}
-
-print(a.pop('t'))  #Pop fucntion requires 1 arguement
-del a[1]
-print(a)
-
-#Program to print words having frequency k
-s= "This is a word string having many many word"
-words=s.split()
-d={}
-for w in words:
-    d[w]=d.get(w, 0)+1
-print(d)
-for w in d:
-    if d[w] == 2:
-        print(w)
-
+mn = input().split()
+m = int(mn[0])
+n = int(mn[1])
+magazine = input().rstrip().split()
+note = input().rstrip().split()
+checkMagazine(magazine, note)
 
