a379f74b9637aab3ed0706fc595c0f359fcd0413
Divij <divij.berry1999@gmail.com>
2020-04-16 23:27:44 +0530

    Hackerank Q

--- Dictionary.py
+++ Dictionary.py
@@ -1,41 +1,41 @@
-# a={1:2,3:4,"list":[1,23],"dict":{1:2}}
-# print(a[1])
-# print(a.get("list"))
-# print(a.get("li", 0)) # If the key is not present then it will return 0. Use of get will not show runtime error
-# print(a.keys())
-# print(a.values())
-# print(a.items()) # it will get key:value pairs
-#
-# print("loop dic")
-# for i in a:
-#     print(i,a[i])
-#
-# for i in a.values():
-#     print(i)
-#
-# print("list" in a) # to check if the key exist or not. It will return TRUE
-#
-# a["t"]=(1,2,3,4)  # To add another key in the dictionary a
-# print(a)
-#
-# b={3:5,'the': 4,2 :100}
-# a.update(b)  # This function will update dictionary a on the basis of b dict
-# print(a) #{1: 2, 3: 5, 'list': [1, 23], 'dict': {1: 2}, 'tuple': (1, 2, 3, 4), 'the': 4, 2: 100}
-#
-# print(a.pop('t'))  #Pop fucntion requires 1 arguement
-# del a[1]
-# print(a)
-#
-# #Program to print words having frequency k
-# s= "This is a word string having many many word"
-# words=s.split()
-# d={}
-# for w in words:
-#     d[w]=d.get(w, 0)+1
-# print(d)
-# for w in d:
-#     if d[w] == 2:
-#         print(w)
+a={1:2,3:4,"list":[1,23],"dict":{1:2}}
+print(a[1])
+print(a.get("list"))
+print(a.get("li", 0)) # If the key is not present then it will return 0. Use of get will not show runtime error
+print(a.keys())
+print(a.values())
+print(a.items()) # it will get key:value pairs
+
+print("loop dic")
+for i in a:
+    print(i,a[i])
+
+for i in a.values():
+    print(i)
+
+print("list" in a) # to check if the key exist or not. It will return TRUE
+
+a["t"]=(1,2,3,4)  # To add another key in the dictionary a
+print(a)
+
+b={3:5,'the': 4,2 :100}
+a.update(b)  # This function will update dictionary a on the basis of b dict
+print(a) #{1: 2, 3: 5, 'list': [1, 23], 'dict': {1: 2}, 'tuple': (1, 2, 3, 4), 'the': 4, 2: 100}
+
+print(a.pop('t'))  #Pop fucntion requires 1 arguement
+del a[1]
+print(a)
+
+#Program to print words having frequency k
+s= "This is a word string having many many word"
+words=s.split()
+d={}
+for w in words:
+    d[w]=d.get(w, 0)+1
+print(d)
+for w in d:
+    if d[w] == 2:
+        print(w)
 
 def checkMagazine(magazine, note):
     dm={}
--- test.py
+++ test.py
@@ -1,56 +1,34 @@
 # t=int(input())
 # for i in range(t):
-#     n = int(input())
-#     li = [int(x) for x in input().split()]
-#     first = 0
-#     last = len(li)-1
-#     flag = 0
-#     count=li.count(1)
-#     print(count)
-#     while(first < last and count>0):
-#         if li[first] == 1:
-#             if li[last] == 1:
-#                 distance = last-first
-#                 if distance >= 6:
-#                     flag = 1
-#                 else:
-#                     flag = 0
-#                     break
-#             last=last-1
+#     n,c,m=map(int, input().split())
+#     num_choc=n//c
+#     c = 0
+#     temp = 2
+#     while(True):
+#         if num_choc == m:
+#             print(num_choc + 1)
+#             break
+#         elif num_choc < m:
+#             print(num_choc)
+#             break
+#         if(temp>1):
+#             c = c + 1
+#             temp = num_choc - m-c
 #         else:
-#             first = first+1
-#     if flag == 1:
-#         print("YES")
-#     else:
-#         print("NO")
-# t=[1,6,12,18]
-# # l=[j-i for i, j in zip(t[:-1], t[1:])]  # or use itertools.izip in py2k
-# # print(l)
-# # li=[]
-# for i,j in zip(t[:-1],t[1:]):
-#     if j-i>=6:
-#         print("Yes")
-#         print(j-i)
-#     else:
-#         print("No")
-#         print(j-i)
-
-t=int(input())
-for i in range(t):
-    n = int(input())
-    li = [int(x) for x in input().split()]
-    res_list = [i for i, value in enumerate(li) if value == 1]
-    if len(res_list)==1:
-        print("YES")
-        continue
-    flag=0
-    for i,j in zip(res_list[:-1],res_list[1:]):
-        if j-i>=6:
-            flag=1
-        else:
-            flag=0
-            break
-    if flag==1:
-        print("YES")
+#             final_choc = num_choc + m * c
+#             print(final_choc)
+#             break
+s="rhbaasdndfsdskgbfefdbrsdfhuyatrjtcrtyytktjjt"
+# s1 = s
+# d1 = {}
+# for i in s1:
+#     d1[i] = d1.get(i, 0) + 1
+# print(d1)
+p = 0
+for e in 'hackerrank':
+    if e in s[p:]:
+        print(e)
+        p = s.index(e,p) + 1
     else:
-        print("NO")
+        print('NO')
+print("Yes")
