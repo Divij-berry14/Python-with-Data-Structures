ee8a2264d122ff1f6f76f02254536beb0e28fbbe
Divij <divij.berry1999@gmail.com>
2020-05-09 03:47:35 +0530

    leetcode

--- Dictionary.py
+++ Dictionary.py
@@ -1,66 +1,137 @@
-a={1:2,3:4,"list":[1,23],"dict":{1:2}}
-print(a[1])
-print(a.get("list"))
-print(a.get("li", 0)) # If the key is not present then it will return 0. Use of get will not show runtime error
-print(a.keys())
-print(a.values())
-print(a.items()) # it will get key:value pairs
+# a={1:2,3:4,"list":[1,23],"dict":{1:2}}
+# print(a[1])
+# print(a.get("list"))
+# print(a.get("li", 0)) # If the key is not present then it will return 0. Use of get will not show runtime error
+# print(a.keys())
+# print(a.values())
+# print(a.items()) # it will get key:value pairs
+#
+# print("loop dic")
+# for i in a:
+#     print(i,a[i])
+#
+# for i in a.values():
+#     print(i)
+#
+# print("list" in a) # to check if the key exist or not. It will return TRUE
+#
+# a["t"]=(1,2,3,4)  # To add another key in the dictionary a
+# print(a)
+#
+# b={3:5,'the': 4,2 :100}
+# a.update(b)  # This function will update dictionary a on the basis of b dict
+# print(a) #{1: 2, 3: 5, 'list': [1, 23], 'dict': {1: 2}, 'tuple': (1, 2, 3, 4), 'the': 4, 2: 100}
+#
+# print(a.pop('t'))  #Pop fucntion requires 1 arguement
+# del a[1]
+# print(a)
+#
+# #Program to print words having frequency k
+# s= "This is a word string having many many word"
+# words=s.split()
+# d={}
+# for w in words:
+#     d[w]=d.get(w, 0)+1
+# print(d)
+# for w in d:
+#     if d[w] == 2:
+#         print(w)
+#
+# def checkMagazine(magazine, note):
+#     dm={}
+#     dn={}
+#     for w in magazine:
+#         dm[w]=dm.get(w,0)+1
+#     for w in note:
+#         dn[w]=dn.get(w,0)+1
+#     for w in dn:
+#         if w in dm and dn[w]<=dm[w]:
+#             flag=1
+#         else:
+#             flag=0
+#             break
+#     print(dm)
+#     print(dn)
+#     if flag==1:
+#         print("Yes")
+#     else:
+#         print("No")
+#
+# mn = input().split()
+# m = int(mn[0])
+# n = int(mn[1])
+# magazine = input().rstrip().split()
+# note = input().rstrip().split()
+# checkMagazine(magazine, note)
 
-print("loop dic")
-for i in a:
-    print(i,a[i])
+# def numJewelsInStones(J,S):
+#     d1 = {}
+#     d2 = {}
+#     for i in J:
+#         d1[i] = d1.get(i, 0) + 1
+#     for j in S:
+#         d2[j] = d2.get(j, 0) + 1
+#     print(d1,d2)
+#     for w in d1:
+#         if w in d2:
+#             if d1[w] == d2[w]:
+#                 return "true"
+#             else:
+#                 return "false"
+#
+# S="aa"
+# J="aab"
+# res=numJewelsInStones(S,J)
+# print(res)
+s="fihjjjjei"
+j="hjibagacbhadfaefdjaeaebgi"
+# d1={}
+# d2={}
+# for i in s:
+#     d1[i] = d1.get(i, 0) + 1
+# for i in j:
+#     d2[i] = d2.get(i, 0) + 1
+#
+# # printing original dictionaries
+# print("The original dictionary 1 : " + str(d1))
+# print("The original dictionary 2 : " + str(d2))
 
-for i in a.values():
-    print(i)
+# Using items() + <= operator
+# Check if one dictionary is subset of other
+# res = d1.items() <= d2.items()
 
-print("list" in a) # to check if the key exist or not. It will return TRUE
-
-a["t"]=(1,2,3,4)  # To add another key in the dictionary a
-print(a)
-
-b={3:5,'the': 4,2 :100}
-a.update(b)  # This function will update dictionary a on the basis of b dict
-print(a) #{1: 2, 3: 5, 'list': [1, 23], 'dict': {1: 2}, 'tuple': (1, 2, 3, 4), 'the': 4, 2: 100}
-
-print(a.pop('t'))  #Pop fucntion requires 1 arguement
-del a[1]
-print(a)
-
-#Program to print words having frequency k
-s= "This is a word string having many many word"
-words=s.split()
+# printing result
+# print("Does dict2 lie in dict1 ? : " + str(res))
+# if len(ransomNote)==0:
+#     return True
+# else:
+#     return False
+# for i in s:
+#     if s.count(i)<=j.count(i):
+#         flag=1
+#     else:
+#         flag=0
+#         break
+# if flag==0:
+#     print("false")
+# else:
+#     print("true")
+li=[int(x) for x in input().split()]
+s=",".join(map(str,li))
+s=s.split(",")
+print(s)
 d={}
-for w in words:
-    d[w]=d.get(w, 0)+1
+for w in s:
+    d[w]=d.get(w,0)+1
 print(d)
+max=0
 for w in d:
-    if d[w] == 2:
-        print(w)
+    if max<d[w]:
+        max=d[w]
+        value=w
+print(value)
 
-def checkMagazine(magazine, note):
-    dm={}
-    dn={}
-    for w in magazine:
-        dm[w]=dm.get(w,0)+1
-    for w in note:
-        dn[w]=dn.get(w,0)+1
-    for w in dn:
-        if w in dm and dn[w]<=dm[w]:
-            flag=1
-        else:
-            flag=0
-            break
-    print(dm)
-    print(dn)
-    if flag==1:
-        print("Yes")
-    else:
-        print("No")
 
-mn = input().split()
-m = int(mn[0])
-n = int(mn[1])
-magazine = input().rstrip().split()
-note = input().rstrip().split()
-checkMagazine(magazine, note)
+
+
 
--- test.py
+++ test.py
@@ -1,42 +1,48 @@
-t=int(input())
-for i in range(t):
-    n,s=map(int,input().split())
-    price=[int(x) for x in input().split()]
-    players=[int(y) for y in input().split()]
-    min_n=min(price)
-    index=price.index(min_n)
-    price.remove(min_n)
-    players.pop(index)
-    for j in range(len(price)):
-        if players[index]==0:
-            if players[j]==1:
-                if s+min_n+price[j]<100:
-                    flag=1
-                    break
-                else:
-                    flag=0
-            else:
-                flag=0
-        else:
-            if players[j]==0:
-                if s+min_n+price[j]<100:
-                    flag=1
-                    break
-                else:
-                    flag=0
-            else:
-                flag=0
-    if flag==1:
-        print("yes")
+a=[[-7,-3],[-7,-1],[-2,-2],[0,-8],[2,-2],[5,-6],[5,-5],[1,7]]
+# for i in range(0,len(a)):
+#     for j in range(0,len(a[i]),2):
+#         x1=a[i][j]
+#         y1=a[i][j+1]
+#         x2=a[i+1][j]
+#         y2=a[i+1][j+1]
+#         print(x1,y1,x2,y2,end=" ")
+#     print()
+for i in range(0,len(a)-1,1):
+    li=a[i]+a[i+1]
+    # print(str(li))
+    x = (li[2] - li[0])
+    y = (li[3] - li[1])
+    if x!=0:
+        div = y / x
     else:
-        print("no")
+        flag=0
+        break
+    print(div)
+    if i==0:
+        slope=div
+    if slope==div:
+        flag=1
+    else:
+        flag=0
+if flag==0:
+    print("false")
+else:
+    print("true")
 
-
-
-
-
-
-
-
-
-
+# a=[[-3,-2],[-1,-2],[2,-2],[-2,-2],[0,-2]]
+# d = {}
+# for elem in a:
+#     try:
+#         d[elem[1]].append(elem[0])
+#     except KeyError:
+#         d[elem[1]] = [elem[0]]
+# print(d)
+#
+# sum=0
+# for w in d.values():
+#     sum=sum+w[0]
+# print(sum)
+# sum1=0
+# for w in d.keys():
+#     sum1=sum1+w
+# print(sum1)
