5791a25c10f665a0896e490732305cc55b4e14f5
Divij <divij.berry1999@gmail.com>
2020-05-12 18:40:45 +0530

    test.py

--- test.py
+++ test.py
@@ -1,49 +1,47 @@
-trust=[[1,3],[1,4],[2,3],[2,4],[4,3]]
-# [[1,3],[2,3],[3,1]]
-for i in range(len(trust)):
-    if i==0:
-        a=trust[i][0]
-        b=trust[i][1]
-        c=b
-        continue
-    a1=trust[i][0]
-    b1=trust[i][1]
-    if a==a1:
-        if b==b1:
-            mark1=b1
-    else:
-        if b==b1:
-            mark1=b1
-        elif b==a1:
-            flag=0
+from datetime import datetime
+from datetime import date
 
-# # for i in range(0,len(a)):
-# #     for j in range(0,len(a[i]),2):
-# #         x1=a[i][j]
-# #         y1=a[i][j+1]
-# #         x2=a[i+1][j]
-# #         y2=a[i+1][j+1]
-# #         print(x1,y1,x2,y2,end=" ")
-# #     print()
-# for i in range(0,len(a)-1,1):
-#     li=a[i]+a[i+1]
-#     # print(str(li))
-#     x = (li[2] - li[0])
-#     y = (li[3] - li[1])
-#     if x!=0:
-#         div = y / x
-#     else:
-#         flag=0
-#         break
-#     print(div)
-#     if i==0:
-#         slope=div
-#     if slope==div:
-#         flag=1
-#     else:
-#         flag=0
-# if flag==0:
-#     print("false")
-# else:
-#     print("true")
-#
+class Loan:
+    def loan_system(self,amount,time,date):
+        month=["Januray","Feburary","March","April","May","June","July","August","September","October","November","December"]
+        date=str(date)
+        date_num=date.split("-")[-1]
+        # print(date_num)
+        print("Loan Creation Date:",date)
+        print("Principal Amount:",amount)
+        print("Number of EMI's:",time)
+        rate=12
+        year=time/12
+        Total_Interest=(amount*rate*year)/100
+        EMI_month=Total_Interest/12
+        Total_Amount_Payable=amount+Total_Interest
+        print("EMI Details:")
+        today=datetime.now()
+        month_name=today.strftime("%B")
+        index = month.index(month_name)-1
+        serial_number = 0
+        Principal_remaining=Total_Amount_Payable
+        year=2020
+        for i in range(time):
+            serial_number+=1
+            index=index+1
+            EMI_month_name=month[index]
+            if EMI_month_name=="December":
+                index=-1
+                year=year+1
+            li = [date_num, EMI_month_name, year]
+            total_emi=principal+EMI_month
+            Principal_remaining=Principal_remaining-total_emi
+            EMI_Date="-".join(map(str,li))
+            print("EMI Number",serial_number,",","Principal EMI:",principal,",","Interest EMI=",EMI_month,
+                                              ",","Total EMI=",total_emi,",",
+                                            "EMI Date:",EMI_Date,",",
+                  "Principal Remaining:",Principal_remaining,end="\n")
+
+print("Enter Loan Amount:",end=" ")
+amount=int(input())
+print("Enter number of months:",end=" ")
+time=int(input())
+today=date.today()
+s=Loan()
+s.loan_system(amount,time,today)
