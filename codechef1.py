483d9624289d427554018fba4bf988d1056c6989
Divij <divij.berry1999@gmail.com>
2020-05-22 15:12:33 +0530

    codechef.py

--- codechef1.py
+++ codechef1.py
@@ -1,19 +1,35 @@
-t=int(input())
-for i in range(t):
-    string=input().split()
-    n,k=int(string[0]),int(string[1])
-    trips=[int(x) for x in input().split()]
-    count=0
-    if(n<k):
-        print("Yes")
-        for j in range(1,k):
-            print(j)
-    for m in li:
-        if(m==-1)
-        count+=1
-    if(count==len(li)):
-        print("Yes")
-        j=0
-        for j in range(1,k):
-            print(j)
-    
+# def countFrequency(string, ch):
+#     count = 0;
+#
+#     for i in range(len(string)):
+#
+#         # Check for vowel
+#         if (string[i] == ch):
+#             count += 1;
+#
+#     return count;
+#
+#
+# def sortArr(string):
+#     n = len(string);
+#
+#     vp = [];
+#
+#     for i in range(n):
+#         vp.append((countFrequency(string, string[i]), string[i]));
+#         print(vp)
+#
+#     vp.sort(reverse=True);
+#     print(vp)
+#     for i in range(len(vp)):
+#         print(vp[i][1], end="");
+#
+# s="aabbbbccc"
+# sortArr(s)
+
+s="abbbbccc"
+d={}
+for w in s:
+    d[w]=d.get(w,0)+1
+print(d)
+print(sorted(d.items(),reverse=True, key=lambda kv: (kv[1], kv[0])))
