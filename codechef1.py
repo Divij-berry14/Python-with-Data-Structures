6e8bc8ea4b9a7f17745f8ea642a8110dd5f0d368
Divij <divij.berry1999@gmail.com>
2020-05-25 20:36:23 +0530

    codechef.py

--- codechef1.py
+++ codechef1.py
@@ -1,35 +1,35 @@
-# def countFrequency(string, ch):
-#     count = 0;
-#
-#     for i in range(len(string)):
-#
-#         # Check for vowel
-#         if (string[i] == ch):
-#             count += 1;
-#
-#     return count;
-#
-#
-# def sortArr(string):
-#     n = len(string);
-#
-#     vp = [];
-#
-#     for i in range(n):
-#         vp.append((countFrequency(string, string[i]), string[i]));
-#         print(vp)
-#
-#     vp.sort(reverse=True);
-#     print(vp)
-#     for i in range(len(vp)):
-#         print(vp[i][1], end="");
-#
-# s="aabbbbccc"
-# sortArr(s)
+def countFrequency(string, ch):
+    count = 0;
 
-s="abbbbccc"
-d={}
-for w in s:
-    d[w]=d.get(w,0)+1
-print(d)
-print(sorted(d.items(),reverse=True, key=lambda kv: (kv[1], kv[0])))
+    for i in range(len(string)):
+
+        # Check for vowel
+        if (string[i] == ch):
+            count += 1;
+
+    return count;
+
+
+def sortArr(string):
+    n = len(string);
+
+    vp = [];
+
+    for i in range(n):
+        vp.append((countFrequency(string, string[i]), string[i]));
+        print(vp)
+
+    vp.sort(reverse=True);
+    print(vp)
+    for i in range(len(vp)):
+        print(vp[i][1], end="");
+
+s="aabbbbccc"
+sortArr(s)
+
+# s="abbbbccc"
+# d={}
+# for w in s:
+#     d[w]=d.get(w,0)+1
+# print(d)
+# print(sorted(d.items(),reverse=True, key=lambda kv: (kv[1], kv[0])))
