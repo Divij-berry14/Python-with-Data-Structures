03cc9093219685692f2183f6bd10bb6ea1dfacdf
Divij <divij.berry1999@gmail.com>
2020-06-06 04:21:12 +0530

    codechef

--- codechef1.py
+++ codechef1.py
@@ -1,31 +1,26 @@
-import math
-def kClosest(points, K):
-    points.sort()
-    print(points)
-    li = []
-    s = 0
-    min=-1
-    if k==1:
-        li.append(points[0])
-        return li
-    a=points[0][0]**2+points[0][1]**2
-    min=math.sqrt(a)
-    s=s+1
-    li.append(points[0])
-    for i in range(1,len(points)):
-        a = points[i][0]**2 + points[i][1]**2
-        # print(a)
-        if math.sqrt(a) <= min and s<K:
-            li.append(points[i])
-            s = s + 1
-            if s == K:
-                break
-            else:
-                min=a
-    del points
-    return li
-
-li=[[-5,4],[-6,-2],[4,8]]
-k=2
-res=kClosest(li,k)
-print(res)
+t=int(input())
+for _ in range(t):
+    n,k=map(int,input().split())
+    li=[int(x) for x in input().split()]
+    s=sum(li)
+    sum1=0
+    for i in range(len(li)):
+        if li[i] > k:
+            sum1=sum1 + k
+        else:
+            sum1=sum1+li[i]
+    # print(sum1)
+    res=s-sum1
+    print(res)
+# t=int(input())
+# for _ in range(t):
+#     s=input()
+#     x=0
+#     i=0
+#     while(i<len(s)-1):
+#         if (s[i]=='x' and s[i+1]=='y') or (s[i]=='y' and s[i+1]=='x'):
+#             x += 1
+#             i += 2
+#         else:
+#             i += 1
+#     print(x)
