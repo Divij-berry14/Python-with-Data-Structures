c39d1874f6197513da8e1e5aadbba01869e7f110
Divij <divij.berry1999@gmail.com>
2020-06-06 04:21:42 +0530

    codechef

--- codechef1.py
+++ codechef1.py
@@ -1,26 +1,26 @@
-t=int(input())
-for _ in range(t):
-    n,k=map(int,input().split())
-    li=[int(x) for x in input().split()]
-    s=sum(li)
-    sum1=0
-    for i in range(len(li)):
-        if li[i] > k:
-            sum1=sum1 + k
-        else:
-            sum1=sum1+li[i]
-    # print(sum1)
-    res=s-sum1
-    print(res)
 # t=int(input())
 # for _ in range(t):
-#     s=input()
-#     x=0
-#     i=0
-#     while(i<len(s)-1):
-#         if (s[i]=='x' and s[i+1]=='y') or (s[i]=='y' and s[i+1]=='x'):
-#             x += 1
-#             i += 2
+#     n,k=map(int,input().split())
+#     li=[int(x) for x in input().split()]
+#     s=sum(li)
+#     sum1=0
+#     for i in range(len(li)):
+#         if li[i] > k:
+#             sum1=sum1 + k
 #         else:
-#             i += 1
-#     print(x)
+#             sum1=sum1+li[i]
+#     # print(sum1)
+#     res=s-sum1
+#     print(res)
+t=int(input())
+for _ in range(t):
+    s=input()
+    x=0
+    i=0
+    while(i<len(s)-1):
+        if (s[i]=='x' and s[i+1]=='y') or (s[i]=='y' and s[i+1]=='x'):
+            x += 1
+            i += 2
+        else:
+            i += 1
+    print(x)
