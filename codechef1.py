887dbc40bcb4e9b85ac00a14ee947dd057d7f73f
Divij <divij.berry1999@gmail.com>
2020-07-10 01:26:22 +0530

    codechef

--- codechef1.py
+++ codechef1.py
@@ -1,101 +1,44 @@
-# t=int(input())
-# for _ in range(t):
-#     n,k=map(int,input().split())
-#     li=[int(x) for x in input().split()]
-#     s=sum(li)
-#     sum1=0
-#     for i in range(len(li)):
-#         if li[i] > k:
-#             sum1=sum1 + k
-#         else:
-#             sum1=sum1+li[i]
-#     # print(sum1)
-#     res=s-sum1
-#     print(res)
-# t=int(input())
-# for _ in range(t):
-#     s=input()
-#     x=0
-#     i=0
-#     while(i<len(s)-1):
-#         if (s[i]=='x' and s[i+1]=='y') or (s[i]=='y' and s[i+1]=='x'):
-#             x += 1
-#             i += 2
-#         else:
-#             i += 1
-#     print(x)
-# t = int(input())
-# for _ in range(t):
-#     n=int(input())
-#     li=[int(x) for x in input().split()]
-#     flag=0
-#     if li[0] == 5:
-#         # sum = 5
-#         # fiveCount=li.count(5)
-#         # print(fiveCount)
-#         fiveCount=1
-#         tenCount=0
-#         fi5teenCount=0
-#         for i in range(1, len(li)):
-#             if li[i] != 5:
-#                 s = li[i]-5
-#                 moneyBack = s//5
-#                 if moneyBack <= fiveCount:
-#                     fiveCount = fiveCount - moneyBack
-#                     flag = 1
-#                 else:
-#                     flag = 0
-#                     # print("NO")
-#                     # break
-#             else:
-#                 fiveCount += 1
-#         if flag == 1:
-#             print("YES")
-#         else:
-#             print("NO")
-#     else:
-#         print("NO")
-# t = int(input())
-# for _ in range(t):
-#     n = int(input())
-#     li = list(map(int,input().split()))
-#     curr,flag = li[0],0
-#     if(curr != 5):
-#         print("NO")
-#         continue
-#     money = [1,0]
-#     for i in range(1,len(li)):
-#         if(li[i] == 5):
-#             money[0]+=1
-#         elif(li[i] == 10):
-#             if(money[0] != 0):
-#                 money[0]-=1
-#             else:
-#                 flag = 1
-#                 break
-#         elif(li[i] == 15):
-#             if(money[1] != 0):
-#                 money[1]-=1
-#             elif(money[0] >= 2):
-#                 money[0]-=2
-#             else:
-#                 flag = 1
-#                 break
-#     if(flag == 1):
-#         print("NO")
-#     else:
-#         print("YES")
-# 
-# t = int(input())
-# for _ in range(t):
-#     n = int(input())
-#     if n%2==0:
-#         n=n//2
-#         n=n-1
-#         print(n//2)
-#     else:
-#         n=n-1
-#         print(n//2)
-# 
-import math
-print(math.pi*2*2)
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    c = 0
+    m = 0
+    for i in range(n):
+        li=[int(x) for x in input().split()]
+        n1=li[0]
+        n11=len(str(n1))
+        n2=li[1]
+        n22=len(str(n2))
+        sum1=0
+        sum2=0
+
+        if n11>1:
+            while(n1>0):
+                sum1=sum1+int(n1%10)
+                n1=int(n1//10)
+        else:
+            sum1=n1
+        if n22>1:
+            while(n2>0):
+                sum2=sum2+int(n2%10)
+                n2=int(n2//10)
+        else:
+            sum2=n2
+        if sum1>sum2:
+            c=c+1
+        elif sum1<sum2:
+            m=m+1
+        else:
+            c=c+1
+            m=m+1
+    if c>m:
+        print("0",c)
+    elif m>c:
+        print("1",m)
+    else:
+        print("2",c)
+
+
+
+
+
