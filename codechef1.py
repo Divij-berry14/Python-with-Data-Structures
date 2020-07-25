03f21021ce83c93bb2be75528f3df1e68819ed9b
Divij <divij.berry1999@gmail.com>
2020-07-25 23:22:30 +0530

    sample test

--- codechef1.py
+++ codechef1.py
@@ -1,19 +1,31 @@
-def qu(customers):
-    l = len(customers)
-    d = {}
-    li = []
-    for i in customers:
-        d[i] = d.get(i, 0) + 1
-    for j in d:
-        per = (d[j] / l) * 100
-        if per > 5:
-            li.append(j)
-    li.sort()
-    return li
-
-customers=["Omega","Alpha","Omega","Alpha","Omega","Alpha","Beta"]
-res=qu(customers)
-print(res)
-
-
-
+# def qu(customers):
+#     l = len(customers)
+#     d = {}
+#     li = []
+#     for i in customers:
+#         d[i] = d.get(i, 0) + 1
+#     for j in d:
+#         per = (d[j] / l) * 100
+#         if per > 5:
+#             li.append(j)
+#     li.sort()
+#     return li
+#
+# customers=["Omega","Alpha","Omega","Alpha","Omega","Alpha","Beta"]
+# res=qu(customers)
+# print(res)
+def mk(x):
+    def mk1():
+        print("Decorated")
+        x()
+    return mk1
+def mk2():
+    print("Ordinary")
+p = mk(mk2)
+p()
+print('{0:.4}'.format(2.0/3))
+import re
+expression="expression"
+name = []
+for j in re.finditer(expression, str):
+    name.append(j.group(1))
