de758c0980a637e4710698b2342ad29b82733fa4
Divij <divij.berry1999@gmail.com>
2020-07-27 19:37:36 +0530

    get max

--- Dictionary.py
+++ Dictionary.py
@@ -39,12 +39,45 @@
 #     if d[w] == 2:
 #         print(w)
 
-c=dict([("the",3),("a",2),("of",34)])
-print(c)
+# c=dict([("the",3),("a",2),("of",34)])
+# print(c)
+# print(c.get("of",0))
+# print(c.get("off",0))
+# d=dict.fromkeys(["abc",4,"of"],1)
+# print(d)
 
-d=dict.fromkeys(["abc",4,"of"],1)
-print(d)
+# def maxFreq(l):
+#     d={}
+#     for i in l:
+#         d[i]=d.get(i,0)+1
+#     print(d)
+#     max=-1
+#     for i in d:
+#         if d[i]>max:
+#             max=d[i]
+#             j=i
+#     return j
+# # Main
+# n=int(input())
+# l=list(int(i) for i in input().strip().split(' '))
+# print(maxFreq(l))
 
-
+def pairSum0(l):
+    d = {}
+    for i in l:
+        d[i] = d.get(i,0)+1
+    print(d)
+    for i in d:
+        if -(i) in d and d[i]>0:
+            temp=d[i]*d[-i]
+            while(temp>0):
+                print(-i,i)
+                d[i]=0
+                d[-i]=0
+                temp-=1
+            # print(d)
+n = int(input())
+l = list(int(i) for i in input().strip().split(' '))
+pairSum0(l)
 
 
