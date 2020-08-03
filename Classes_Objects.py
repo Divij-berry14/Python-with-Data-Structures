aa570ecc17d1168be457d7445529221761151ba8
Divij <divij.berry1999@gmail.com>
2020-08-03 22:52:12 +0530

    sample leetcode

--- Classes_Objects.py
+++ Classes_Objects.py
@@ -276,3 +276,49 @@ object1 = D()
 
 # produces an error as d is private instance variable
 print(object1.d)
+
+
+
+# Python program to demonstrate
+# super function
+class Animals:
+
+    # Initializing constructor
+    def __init__(self):
+        self.legs = 4
+        self.domestic = True
+        self.tail = True
+        self.mammals = True
+
+    def isMammal(self):
+        if self.isMammal:
+            print("It is a mammal.")
+
+    def isDomestic(self):
+        if self.mammals:
+            print("It is a domestic animal.")
+
+
+class Dogs(Animals):
+    def __init__(self):
+        super().__init__()
+
+    def isMammal(self):
+        super().isMammal()
+
+
+class Horses(Animals):
+    def __init__(self):
+        super().__init__()
+
+    def hasTailandLegs(self):
+        if self.tail and self.legs == 4:
+            print("Has legs and tail")
+
+        # Dirver code
+
+
+Tom = Dogs()
+Tom.isMammal()
+Bruno = Horses()
+Bruno.hasTailandLegs()
--- test.py
+++ test.py
@@ -1,35 +1,16 @@
-from collections import Counter
-
-
-def removeChars(str1, str2):
-    # make dictionaries from both strings
-    dict1 = Counter(str1)
-    dict2 = Counter(str2)
-    print(dict1)
-    print(dict2)
-    # extract keys from dict1 and dict2
-    keys1 = dict1.keys()
-    keys2 = dict2.keys()
-
-    # count number of keys in both lists of keys
-    count1 = len(keys1)
-    count2 = len(keys2)
-
-    # convert list of keys in set to find common keys
-    set1 = set(keys1)
-    print(set1)
-    commonKeys = len(set1.intersection(keys2))
-    print(commonKeys)
-
-    if (commonKeys == 0):
-        return count1 + count2
+def reverse(s):
+    bad_chars = [';', ':', '!', "*", ",", "."]
+    s = s.lower()
+    s = ''.join(e for e in s if e.isalnum())
+    # for i in bad_chars:
+    #     s=s.replace(i,"")
+    s = s.replace(" ", "")
+    string = "".join(reversed(s))
+    if s == string:
+        return True
     else:
-        return (max(count1, count2) - commonKeys)
-
-    # Driver program
+        return False
 
 
-if __name__ == "__main__":
-    str1 = 'bcadeh'
-    str2 = 'hea'
-    print(removeChars(str1, str2))
+s = "race a car"
+print(reverse(s))
