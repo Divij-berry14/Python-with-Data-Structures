72f6b9f84f919f3f9714d67301a9d47f293a6c93
Divij <divij.berry1999@gmail.com>
2020-08-07 16:38:50 +0530

    Shallow copy and deep copy

--- Code_Chef.py
+++ Code_Chef.py
@@ -1,5 +1,14 @@
-s="geek"
-q=101
-m=hash(s)
-index=m%q
-print(index)
+import copy
+li1=[[1,2],3,4]
+li2=copy.copy(li1)
+print(li1)
+print(li2)
+li2.append(5)
+print(li1)
+print(li2)
+li2[0][1]="a"
+print(li1)
+print(li2)
+li2[0].append("new")
+print(li1)
+print(li2)
