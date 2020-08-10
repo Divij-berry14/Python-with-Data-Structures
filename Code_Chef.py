7eb952b67f680614a57314624fad7ab01f631589
Divij <divij.berry1999@gmail.com>
2020-08-10 18:41:17 +0530

    Regex

--- Code_Chef.py
+++ Code_Chef.py
@@ -1,14 +1,19 @@
-import copy
-li1=[[1,2],3,4]
-li2=copy.copy(li1)
-print(li1)
-print(li2)
-li2.append(5)
-print(li1)
-print(li2)
-li2[0][1]="a"
-print(li1)
-print(li2)
-li2[0].append("new")
-print(li1)
-print(li2)
+s=input()
+l=len(s)
+if l>1:
+    num=0
+    temp=l
+    for i in range(l-1):
+        temp1=ord(s[i])-64
+        # print("temp1",temp1)
+        num=num+temp1*26**(temp-1)
+        # print("num",num)
+        temp=temp-1
+        # print("temp",temp)
+    temp = s[-1]
+    res = num + ord(temp)-64
+    print(res)
+else:
+    print(ord(s)-64)
+
+
---
+++ PythonRegex.py
@@ -0,0 +1,19 @@
+import re
+# match=re.match("na","nanp")
+# print(match)
+# print(match.group())
+# string="This is a pet:cat I love cats"
+# match=re.search(r"c\w\w\w",string)
+# print(match)
+# print(match.group())
+# string=re.search('\w\w\w\w\we','centre')
+# print(string)
+# print(string.group())
+# match=re.search('aa?yushi?','ayush ayushi') #search for the pattern. In this pattern, the ? checks the previous preceding
+# # character and checks if present in the string or not. ? do Matches zero or one occurrence. And in this, the string are
+# # ayush and ayushi but the the pattern has aayushi so, ? matches zero or one occurrence, so the starting second a will not the
+# #counted and check for the pattern. Output will be ayushi
+# print(match.group())
+match=re.findall(r'advi[cs]e','I could advise you on your poem, but you would disparage my advice')
+for i in match:
+    print(i)
