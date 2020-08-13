4b2468a89dec05e7463cd908b3dfc2bee2fc86c6
Divij <divij.berry1999@gmail.com>
2020-08-13 20:29:20 +0530

    Linked List

--- Linked List/Linked_List.py
+++ Linked List/Linked_List.py
@@ -2,84 +2,142 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-    
-    
-a = Node(12)
-b = Node(17)
-a.next = b
-print(a)
-print(a.next)
-print(a.data)
-print(a.next.data)
-print(b)
-print(b.next)
-print(b.data)    
 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-def printLL(head):
+def LengthLL(head):
+    count = 0
     while head is not None:
-        print(head.data, end=" ")
+        count += 1
         head = head.next
+    return count
 
+def LenghtLLRec(head):
+    if head is None:
+        return 0
+    p=LenghtLLRec(head.next)+1
+    return p
 
-
-node1 = Node(10)
-node2 = Node(20)
-node3 = Node(30)
-node4 = Node(40)
-node1.next = node2
-node2.next = node3
-node3.next = node4
-printLL(node2)
-
-
-
-
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-def ithNode(head, i):
-    #############################
-    # PLEASE ADD YOUR CODE HERE #
-    #############################
-    count=0
-    curr=head
+def InsertNodeLL(head,data,i):
+    if i < 0 or i > LenghtLLRec(head):
+        return head
+    if data == None:
+        return head
+    newNode=Node(data)
+    curr = head
+    prev = None
+    count = 0
+    if i == 0:
+        newNode.next=head
+        head = newNode
+        curr = None
     while(curr is not None):
-        if(count<i and curr!=None):
-            curr=curr.next
-            count=count+1
-    return curr
-        
-        
-        
-
-def ll(arr):
-    if len(arr)==0:
-        return None
-    head = Node(arr[0])
-    last = head
-    for data in arr[1:]:
-        last.next = Node(data)
-        last = last.next
+        if count == i:
+            prev.next = newNode
+            newNode.next = curr
+            break
+        prev = curr
+        curr = curr.next
+        count += 1
     return head
 
-# Main
-# Read the link list elements including -1
-arr=list(int(i) for i in input().strip().split(' '))
-# Create a Linked list after removing -1 from list
-l = ll(arr[:-1])
-i=int(input())
-node = ithNode(l, i)
-if node:
-    print(node.data)
+def InputNodeRec(head,data,i):
+    if head is None:
+        return None
+    if i == 0:
+        newNode = Node(data)
+        newNode.next = head
+        p = newNode
+        return p
+    p =  InsertNodeLL(head.next,data,i-1)
+    head.next = p
+    return head
 
-        
-        
-        
-        
-        
+def DeleteNode(head,i):
+    if i < 0 or i > LengthLL(head):
+        return head
+    curr = head
+    prev = None
+    count = 0
+    if i == 0:
+        head = curr.next
+        curr = None
+        return head
+    while curr is not None:
+        if count == i:
+            prev.next = curr.next
+            break
+        prev = curr
+        curr = curr.next
+        count = count+1
+    return head
+
+def AppendLastToFirst(head,n):
+    if head is None:
+        return None
+    curr = head
+    prev = None
+    length = LenghtLLRec(head)
+    count = 1
+    while curr is not None:
+        if count == n:
+            temp1 = prev.next
+            prev.next = None
+        prev = curr
+        curr = curr.next
+        count += 1
+    curr = head
+    curr1 = temp1
+    while curr1.next is not None:
+        curr1 = curr1.next
+    curr1.next = curr
+    return temp1
+
+def RemoveDuplicatesLL(head):
+    if head is None:
+        return None
+    curr = head
+    while curr is not None and curr.next is not None:
+        if curr.data == curr.next.data:
+            # curr.next = None
+            curr.next = curr.next.next
+        else:
+            curr = curr.next
+    return head
+
+def printLL(head):
+    count = 0
+    while head is not None:
+        print(head.data,"->",end=" ")
+        head = head.next
+    print("None")
+
+
+def InputLL():
+    Inputli = [int(x) for x in input().split()]
+    head = None
+    tail = None
+    for currdata in Inputli:
+        if currdata == -1:
+            break
+        node = Node(currdata)
+        if head == None:
+            head = node
+            tail = node
+        else:
+            tail.next = node
+            tail = node
+
+    return head
+
+head = InputLL()
+printLL(head)
+# head = DeleteNode(head,3)
+# printLL(head)
+# print(LenghtLLRec(head))
+# head = InsertNodeLL(head, 9, 5)
+# printLL(head)
+# head = InputNodeRec(head, 9, 3)
+# printLL(head)
+# head = AppendLastToFirst(head,4)
+# printLL(head)
+head = RemoveDuplicatesLL(head)
+printLL(head)
