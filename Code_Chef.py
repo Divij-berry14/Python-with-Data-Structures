fbcccf1c7f74c53dbd52ea8f81f26cf193c2ec37
Divij <divij.berry1999@gmail.com>
2020-08-14 17:54:31 +0530

    Closures

---
+++ Code_Chef.py
@@ -0,0 +1,3 @@
+s="strign"
+for i in s:
+    print(i)
--- PythonRegex.py
+++ PythonRegex.py
@@ -9,11 +9,13 @@ import re
 # string=re.search('\w\w\w\w\we','centre')
 # print(string)
 # print(string.group())
-# match=re.search('aa?yushi?','ayush ayushi') #search for the pattern. In this pattern, the ? checks the previous preceding
+match=re.search('aa?yushi?','ayushi ayush ') #search for the pattern. In this pattern, the ? checks the previous preceding
 # # character and checks if present in the string or not. ? do Matches zero or one occurrence. And in this, the string are
 # # ayush and ayushi but the the pattern has aayushi so, ? matches zero or one occurrence, so the starting second a will not the
 # #counted and check for the pattern. Output will be ayushi
-# print(match.group())
-match=re.findall(r'advi[cs]e','I could advise you on your poem, but you would disparage my advice')
-for i in match:
-    print(i)
+print(match.group())
+# match=re.findall(r'advi[cs]e','I could advise you on your poem, but you would disparage my advice')
+# for i in match:
+#     print(i)
+match=re.search(r'[\w]+@[\w]+\.[\w]+','Please mail it to ayushiwasthere@gmail.com')
+print(match.group())
