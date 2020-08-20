b5e270a7422c98b4f9d536b983882ceff0d7a765
Divij <divij.berry1999@gmail.com>
2020-08-20 18:59:14 +0530

    Linked list

--- Linked List/Reverse_LL.py
+++ Linked List/Reverse_LL.py
@@ -17,45 +17,45 @@ def reverse_LL_rec(head):
     print(head.data, end=' ')
 #    
 def reverse_LL(head):
-    prev=None
-    curr=head
+    prev = None
+    curr = head
     while(curr is not None):
-        next=curr.next
-        curr.next=prev
-        prev=curr
-        curr=next
-    head=prev
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    head = prev
     return head
 
 def reverse_LL2_rec(head):
     if head is None or head.next is None:
         return head
     
-    smallhead=reverse_LL2_rec(head.next)
-    curr=smallhead
+    smallhead = reverse_LL2_rec(head.next)
+    curr = smallhead
     while curr.next is not None:
-        curr=curr.next
-    curr.next=head
-    head.next=None
+        curr = curr.next
+    curr.next = head
+    head.next = None
     return smallhead
 
 def reverse_LL3_rec_Optimized(head):
     
     if head is None or head.next is None:
-        return head,head
-    smallhead,smalltail=reverse_LL3_rec_Optimized(head.next)
-    smalltail.next=head
-    head.next=None
-    return smallhead,head
+        return head, head
+    smallhead, smalltail = reverse_LL3_rec_Optimized(head.next)
+    smalltail.next = head
+    head.next = None
+    return smallhead, head
 
 def reverse_LL4_rec_Optimized(head):
     
     if head is None or head.next is None:
         return head
-    smallhead=reverse_LL4_rec_Optimized(head.next)
-    tail=head.next
-    tail.next=head
-    head.next=None
+    smallhead = reverse_LL4_rec_Optimized(head.next)
+    tail = head.next
+    tail.next = head
+    head.next = None
     return smallhead
 
 
