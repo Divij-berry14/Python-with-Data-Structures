0109268b6cc8cd270f700c0c55a96c0889125248
Divij <divij.berry1999@gmail.com>
2020-08-23 17:36:42 +0530

    ZS

--- Code_Chef.py
+++ Code_Chef.py
@@ -1,2 +1,28 @@
-print(type(range(6)))
-print(range(6))
+string1 = input()
+string2 = input()
+k = int(input())
+res=""
+new_li=[]
+for i in range(len(string1)):
+    if ord(string1[i])+k>=ord("z"):
+        temp1 = ord("z")-ord(string1[i])
+        temp2 = ord(string2[i])
+        if abs(temp1 - temp2) <= k:
+            flag = True
+        else:
+            flag = False
+    else:
+        temp1 = ord(string1[i])
+        temp2 = ord(string2[i])
+        if abs(temp1 -temp2) <= k:
+            flag = True
+        else:
+            flag = False
+
+if flag:
+    print("Yes")
+else:
+    print("No")
+
+
+
