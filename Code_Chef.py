84e8e7b1d3eb43d5902d78ebb96fbd288fd27078
Divij <divij.berry1999@gmail.com>
2020-09-07 20:31:44 +0530

    codechef

--- Code_Chef.py
+++ Code_Chef.py
@@ -8,9 +8,13 @@
 # print(list(res))
 # print(list(map(lambda x:x%2==0,range(0,10))))
 # print(list(filter(lambda x:x*2,range(0,10))))
-def func(li):
-    li.append(8)
-
-li=[1,5]
-func(li)
-print(li)
+# def func(li):
+#     li.append(8)
+#
+# li=[1,5]
+# func(li)
+# print(li)
+s1 = "dog is"
+s2 = "aa"
+print(len(s1))
+print(len(s2))
