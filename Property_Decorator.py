066f4bc6af9db8377a308f2f2b8ea7e59d172e8f
Divij <divij.berry1999@gmail.com>
2020-09-19 16:33:52 +0530

    Property_Decorator.py

--- Property_Decorator.py
+++ Property_Decorator.py
@@ -1,13 +1,35 @@
-class Student:
-    def __init__(self, grade, name):
-        self.name = name
-        self.grade = grade
+class Employee:
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+        # self.email = first + '.' + last + '@email.com'
 
-    def msg(self):
-        return self.name + " got grade " + self.grade
+    @property
+    def email(self):
+        return self.first + '.' + self.last + '@email.com'
 
-stud1 = Student("B", "Davij")
-stud1.grade = "A"
-print("Name:", stud1.name)
-print("Grade:", stud1.grade)
-print("Message:", stud1.msg())
+    @email.setter
+    def email(self, mail):
+        names = mail.split("@")[0]
+        self.first = names.split(".")[0]
+        self.last = names.split(".")[1]
+
+    @property
+    def fullName(self):  #getter method
+        return '{} {}'.format(self.first, self.last)
+
+    @fullName.setter
+    def fullName(self, name): #setter method
+        first, last = name.split(" ")
+        self.first = first
+        self.last = last
+
+emp1 = Employee('divij', 'berry')
+emp1.first = "davij"
+emp1.last = "berr"
+emp1.email = "div.berry1999@gmailcom"
+# emp1.fullName = "jimmy salvatore"
+print(emp1.first)
+print(emp1.last)
+# print(emp1.fullName)
+print(emp1.email)
