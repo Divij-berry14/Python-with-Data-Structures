ff1e50755cbb231ec723f2aaccc566ddb602bdc8
Divij <divij.berry1999@gmail.com>
2020-09-19 17:23:35 +0530

    Property_Decorator.py

--- Property_Decorator.py
+++ Property_Decorator.py
@@ -6,6 +6,8 @@ class Employee:
 
     @property
     def email(self):
+        if self.first == None and self.last == None:
+            return "Email is not set. Please set it using setter"
         return self.first + '.' + self.last + '@email.com'
 
     @email.setter
@@ -13,6 +15,11 @@ class Employee:
         names = mail.split("@")[0]
         self.first = names.split(".")[0]
         self.last = names.split(".")[1]
+
+    @email.deleter
+    def email(self):
+        self.first = None
+        self.last = None
 
     @property
     def fullName(self):  #getter method
@@ -25,11 +32,17 @@ class Employee:
         self.last = last
 
 emp1 = Employee('divij', 'berry')
+print("before Mail-", emp1.email)
 emp1.first = "davij"
 emp1.last = "berr"
-emp1.email = "div.berry1999@gmailcom"
+print("after Mail-", emp1.email)
+print("after Full Name-", emp1.fullName)
+emp1.email = "div1.berry999@gmailcom"
 # emp1.fullName = "jimmy salvatore"
-print(emp1.first)
-print(emp1.last)
+print("new-", emp1.first)
+print("new-", emp1.last)
 # print(emp1.fullName)
-print(emp1.email)
+print("new-", emp1.email)
+print("new-", emp1.fullName)
+del emp1.email
+print("delete email-", emp1.email)
