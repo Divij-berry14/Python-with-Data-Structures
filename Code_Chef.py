92eb86f1e54aab4ab2d9c6704f7e63476d4fe6cc
Divij <divij.berry1999@gmail.com>
2020-10-03 01:26:07 +0530

    A Boolean Matrix Question.py

--- Code_Chef.py
+++ Code_Chef.py
@@ -24,39 +24,10 @@
 # fun(li)
 # print(li)
 
-s = " geeks quiz practice code"
-s1 = s.split(" ")
-print(s1)
-res = ""
-for i in range(len(s1)-1, -1, -1):
-    res = res + " " + s1[i]
-print(res)
-
-# Python3 program to reverse a string
-# s = input()
-# s = "i like this program very much"
-# words = s.split(' ')
-# string =[]
-# for word in words:
-# 	string.insert(0, word)
-# print(string)
-# print("Reversed String:")
-# print(" ".join(string))
 
 
-# Recursive python program to reverse an array
 
-# Function to reverse A[] from start to end
-# def reverseList(A, start, end):
-# 	if start >= end:
-# 		return
-# 	A[start], A[end] = A[end], A[start]
-# 	return reverseList(A, start+1, end-1)
-#
-# # Driver function to test above function
-# A = [1, 2, 3, 4, 5, 6]
-# print(A)
-# A = reverseList(A, 0, 5)
-# print("Reversed list is")
-# print(A)
+
+
+
 
--- Reverse_String.py
+++ Reverse_String.py
@@ -1,8 +1,53 @@
 s = "geeks quiz practice code"
 s1 = s.split(" ")
 res = ""
-for i in range(len(s1)-1, -1, -1):
+print(s1)
+print(s1[::-1])
+for i in range(len(s1)-1, 0, -1):
     print(i)
     res = res + s1[i] + " "
 
 print(res)
+
+# def reverseList(li):
+#     start = 0
+#     end = len(li) - 1
+#     while start < end:
+#         li[start], li[end] = li[end], li[start]
+#         start += 1
+#         end -= 1
+#     print(li)
+#
+# A = [1,2,3,4,5,6]
+# reverseList(A)
+
+
+
+# Python3 program to reverse a string
+# s = input()
+# s = "i like this program very much"
+# words = s.split(' ')
+# string =[]
+# for word in words:
+# 	string.insert(0, word)
+# print(string)
+# print("Reversed String:")
+# print(" ".join(string))
+
+
+
+# Recursive python program to reverse an array
+
+# Function to reverse A[] from start to end
+# def reverseList(A, start, end):
+# 	if start >= end:
+# 		return
+# 	A[start], A[end] = A[end], A[start]
+# 	return reverseList(A, start+1, end-1)
+#
+# # Driver function to test above function
+# A = [1, 2, 3, 4, 5, 6]
+# print(A)
+# A = reverseList(A, 0, 5)
+# print("Reversed list is")
+# print(A)
