c912077535ce0fe0d63de7387066a2ccc7fe367c
Divij <divij.berry1999@gmail.com>
2020-10-11 01:17:35 +0530

    sor

--- Sort_0_1.py
+++ Sort_0_1.py
@@ -7,17 +7,17 @@
 #        if(li[i]<li[j]):
 #            print(li[i],end=' ')
 
-n=input()
-li=[int(x) for x in input().split()]
+n = input()
+li = [int(x) for x in input().split()]
 li.sort()
 for i in li:
-    print(i,end=' ')
+    print(i, end=' ')
             
-n=int(input())
-li=[int(x) for x in input().split()]
-li.sort(reverse = True)
-min=li[0]
-for i in range(1,n):
-   if(min>li[i]):
+n = int(input())
+li = [int(x) for x in input().split()]
+li.sort(reverse=True)
+min = li[0]
+for i in range(1, n):
+   if(min > li[i]):
        print(li[i])
        break
