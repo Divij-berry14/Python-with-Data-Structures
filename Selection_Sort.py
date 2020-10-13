42350ac7b0a82e87ce3fcda8ca4b6063fbfb4fda
Divij <divij.berry1999@gmail.com>
2020-10-13 23:28:50 +0530

    Selection_Sort.py

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -1,14 +1,22 @@
+# def Selection_Sort(li):
+#     length = len(li)
+#     for i in range(length-1):
+#         minIndex = i
+#         for j in range(i+1, length):
+#             if(li[minIndex] > li[j]):
+#                 minIndex = j
+#         li[i], li[minIndex] = li[minIndex], li[i]
+#         print(li)
+    
 def Selection_Sort(li):
-    length = len(li)
-    for i in range(length-1):
+    l = len(li)
+    for i in range(l):
         minIndex = i
-        for j in range(i+1, length):
-            if(li[minIndex] > li[j]):
+        for j in range(i+1,l):
+            if li[minIndex] > li[j]:
                 minIndex = j
         li[i], li[minIndex] = li[minIndex], li[i]
-        print(li)
-    
-
+    print(li)
 #li=[1,3,2,4,0,6,8]
 li = [int(x) for x in input().split()]
 Selection_Sort(li)
