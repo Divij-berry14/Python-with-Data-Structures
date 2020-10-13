cd87cd772a99cbab7f879c29a7db7bcc12fc0fd5
Divij <divij.berry1999@gmail.com>
2020-10-13 23:46:38 +0530

    Insertion_Sort.py

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -10,18 +10,27 @@
 #     for i in li:
 #         print(i, end=' ')
 # li = [int(x) for x in input().split()]
+# def InsertionSort(li):
+#     length = len(li)
+#     for i in range(1, length):
+#         j = i - 1
+#         temp = li[i]
+#         while j >= 0 and li[j] > temp:
+#             li[j+1] = li[j]
+#             j = j - 1
+#             print(j, li)
+#         li[j+1] = temp
+#         print(li)
 def InsertionSort(li):
-    length = len(li)
-    for i in range(1, length):
+    l = len(li)
+    for i in range(1, l):
         j = i - 1
         temp = li[i]
-        while j >= 0 and li[j] > temp:
+        while(j>=0 and li[j] > temp):
             li[j+1] = li[j]
             j = j - 1
-            print(j, li)
         li[j+1] = temp
-        print(li)
-
+    print(li)
 li = [int(x) for x in input().split()]
 InsertionSort(li)
 print(li)
