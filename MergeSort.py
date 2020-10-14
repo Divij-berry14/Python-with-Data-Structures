195150c081a2a2990fb6e5041f6df2346a13d6d9
Divij <divij.berry1999@gmail.com>
2020-10-14 20:43:06 +0530

    MergeSort.py

--- MergeSort.py
+++ MergeSort.py
@@ -49,7 +49,7 @@ def mergeSort(arr, l, r):
     if l < r:
         # Same as (l+r)//2, but avoids overflow for
         # large l and h
-        m = (l + (r - 1)) // 2
+        m = (l + r) // 2
 
         # Sort first and second halves
         mergeSort(arr, l, m)
@@ -66,6 +66,7 @@ n = len(arr)
 #     print("%d" % arr[i]),
 
 mergeSort(arr, 0, n - 1)
-print("\n\nSorted array is")
-for i in range(n):
-    print("%d" % arr[i]),
+print(arr)
+# print("\n\nSorted array is")
+# for i in range(n):
+#     print("%d" % arr[i])
