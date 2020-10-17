a57145a3378acd48f0ff8282de6b426707a702fa
Divij <divij.berry1999@gmail.com>
2020-10-17 23:25:36 +0530

    test1.py

--- Stacks and Queue/test1.py
+++ Stacks and Queue/test1.py
@@ -1,31 +1,48 @@
+# class Queue:
+#     def __init__(self):
+#         self.arr = []
+#         self.front = 0
+#         self.count = 0
+#
+#     def enqueue(self, data):
+#         self.arr.append(data)
+#         self.count += 1
+#
+#     def dequeue(self):
+#         if self.count == 0:
+#             return -1
+#         element = self.arr[self.front]
+#         self.front += 1
+#         self.count -= 1
+#         return element
+#
+#     def Front(self):
+#         if self.count == 0:
+#             return -1
+#         return self.arr[self.front]
+#
+#     def size(self):
+#         return self.count
+#
+#     def isEmpty(self):
+#         return self.count == 0
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
 class Queue:
     def __init__(self):
-        self.arr = []
-        self.front = 0
+        self.head = None
+        self.tail = None
         self.count = 0
 
     def enqueue(self, data):
-        self.arr.append(data)
-        self.count += 1
-
-    def dequeue(self):
-        if self.count == 0:
-            return -1
-        element = self.arr[self.front]
-        self.front += 1
-        self.count -= 1
-        return element
-
-    def Front(self):
-        if self.count == 0:
-            return -1
-        return self.arr[self.front]
-
-    def size(self):
-        return self.count
-
-    def isEmpty(self):
-        return self.count == 0
+        node = Node(data)
+        if self.head == None and self.tail == None:
+            self.head = node
+            self.tail = node
 
 q = Queue()
 q.enqueue(1)
