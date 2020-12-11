2d44dda467cedd4ef37194819a4791b7915148f5
Divij <divij.berry1999@gmail.com>
2020-12-11 21:18:17 +0530

    Count frequencies of all elements in O(1) space and O(n) time.py

--- Count frequencies of all elements in O(1) space and O(n) time.py
+++ Count frequencies of all elements in O(1) space and O(n) time.py
@@ -1,16 +1,17 @@
-# def duplicateElement(arr):
-#     res = []
-#     for i in range(len(arr)):
-#         temp = arr[i]
-#         if arr[abs(temp)] > 0:
-#             arr[abs(temp)] *= -1
-#         else:
-#             res.append(abs(arr[i]))
-#         print(arr)
-#     print(res)
-#
-# arr = [1, 2, 3, 1, 3, 6, 6, 6]
-# duplicateElement(arr)
+def duplicateElement(arr):
+    res = []
+    for i in range(len(arr)):
+        temp = arr[i]
+        if arr[abs(temp)] > 0:
+            arr[abs(temp)] *= -1
+        else:
+            res.append(abs(arr[i]))
+        print(arr)
+        print(res)
+    print(res)
+
+arr = [1, 2, 3, 1, 3, 6, 6, 6]
+duplicateElement(arr)
 
 
 
