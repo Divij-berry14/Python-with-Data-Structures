b3a6db54aabf4c2d1b8d19d353c5fc97b2485dae
Divij <divij.berry1999@gmail.com>
2020-12-24 22:00:16 +0530

    URL Shorten.py

--- dgdg.py
+++ dgdg.py
@@ -1,138 +1,102 @@
-# def min1(li):
-#     l=len(li)
-#     li.sort()
-#     li1=[]
-#     for i in range(l):
-#         j=i+1
-#         for j in range(l-1):
-#             if(li[i]%2==0):
-#                 temp=li[i]//2
-#                 diff=abs(temp-li[j])
-#                 li1.append(diff)
-#     m=min(li1)
-#     return m
+from cffi.backend_ctypes import xrange
+
 #
+# Converts any integer into a base [BASE] number. I have chosen 62
+# as it is meant to represent the integers using all the alphanumeric
+# characters, [no special characters] = {0..9}, {A..Z}, {a..z}
 #
-# li=list(map(int, input.split()))
-# out=min1(li)
-# print(out)
-# class Person:
-#   def __init__(self, id):
-#     self.id = id
+# I plan on using this to shorten the representation of possibly long ids,
+# a la url shortenters
 #
-# sam = Person(100)
+# saturate()  takes the base 62 key, as a string, and turns it back into an integer
+# dehydrate() takes an integer and turns it into the base 62 string
 #
-# sam.__dict__['age'] = 49
-#
-# print(sam.age + len(sam.__dict__))
-#
-# def my_func(num):
-#   data = [0]
-#   for i in range(1, num+1):
-#     data.append(data[i >> 1] + int(i & 1))
-#   return data
-#
-#
-# num = 6
-# print(my_func(num))
-#
-# val1=28**0
-# val2=2.5
-# val3='123'
-# val4=int(val3)
-# print(val1+val2+val4)
+import math
+import sys
 
-# def prime(num):
-#   res = []
-#   temp = 2
-#   for i in range(2, num+1):
-#     if i%2 == 0:
-#       continue
-#     else:
-#       res.append(i)
-#
-#   for i in res:
-#     print(i, end=" ")
-#
-# prime(10)
+BASE = 62
 
-# class Test:
-#   def add(self, v1, v2):
-#     ans = v1+v2
-#     print(ans)
-#
-# obj = Test()
-# obj.add(1, 2)
-
-# def sum(v1,v2):
-#   print(v1+v2)
-#
-# sum(1,2)
-# print(v1,v2)
+UPPERCASE_OFFSET = 55
+LOWERCASE_OFFSET = 61
+DIGIT_OFFSET = 48
 
 
-# Python program to show that we can create
-# instance variables inside methods
+def true_ord(char):
+    """
+    Turns a digit [char] in character representation
+    from the number system with base [BASE] into an integer.
+    """
 
-# Class for Computer Science Student
-
-# Python example to show the working of multiple
-# inheritance
-
-# Python program to
-# demonstrate protected members
-
-
-# import re
-#
-# number = int(input())
-#
-# for n in range(number):
-#     string = input()
-#     if re.search(r'^(([01]?\d?\d|2[0-4]\d|25[0-5])\.){3}([01]?\d?\d|2[0-4]\d|25[0-5])$', string):
-#         print('Valid')
-#     else:
-#         print('Invalid')
-#
-#
-# import operator
-# n = int(input())
-# arr = [int(x) for x in input().split()]
-# d = {}
-# li = []
-# for i in arr:
-#     d[i] = d.get(i, 0) + 1
-# print(d)
-# sorted_d = dict(sorted(d.items(), key= lambda kv:(kv[1],kv[0]),reverse=True))
-# print(sorted_d)
-# for i in sorted_d:
-#     li.append(i)
-# print(li)
-# # for i in sorted_d:
-
-def fun(n):
-    b=int(pow(n,0.5))
-    s=0
-    for i in range(1,b+1):
-        s=s+n//i
-    s=2*s-(b*b)
-    return s
-
-t = int(input())
-for i in range(t):
-    arr = [int(x) for x in input().split()]
-    sum1 = 0
-    for j in arr:
-        temp = fun(j)
-        sum1 = sum1 + temp
-    print(sum1)
+    if char.isdigit():
+        return ord(char) - DIGIT_OFFSET
+    elif 'A' <= char <= 'Z':
+        return ord(char) - UPPERCASE_OFFSET
+    elif 'a' <= char <= 'z':
+        return ord(char) - LOWERCASE_OFFSET
+    else:
+        raise ValueError("%s is not a valid character" % char)
 
 
+def true_chr(integer):
+    """
+    Turns an integer [integer] into digit in base [BASE]
+    as a character representation.
+    """
+    if integer < 10:
+        return chr(integer + DIGIT_OFFSET)
+    elif 10 <= integer <= 35:
+        return chr(integer + UPPERCASE_OFFSET)
+    elif 36 <= integer < 62:
+        return chr(integer + LOWERCASE_OFFSET)
+    else:
+        raise ValueError("%d is not a valid integer in the range of base %d" % (integer, BASE))
 
 
+def saturate(key):
+    """
+    Turn the base [BASE] number [key] into an integer
+    """
+    int_sum = 0
+    reversed_key = key[::-1]
+    for idx, char in enumerate(reversed_key):
+        int_sum += true_ord(char) * int(math.pow(BASE, idx))
+    return int_sum
 
 
+def dehydrate(integer):
+    """
+    Turn an integer [integer] into a base [BASE] number
+    in string representation
+    """
+
+    # we won't step into the while if integer is 0
+    # so we just solve for that case here
+    if integer == 0:
+        return '0'
+
+    string = ""
+    while integer > 0:
+        remainder = integer % BASE
+        string = true_chr(remainder) + string
+        integer = int(integer / BASE)
+    return string
 
 
-
-
+if __name__ == '__main__':
+    # not really unit tests just a rough check to see if anything is way off
+    if sys.argv[1] == '-tests':
+        passed_tests = True
+        for i in xrange(0, 1000):
+            passed_tests &= (i == saturate(dehydrate(i)))
+        print(passed_tests)
+    else:
+        user_input = sys.argv[2]
+        try:
+            if sys.argv[1] == '-s':
+                print(saturate(user_input))
+            elif sys.argv[1] == '-d':
+                print(dehydrate(int(user_input)))
+            else:
+                print("I don't understand option %s" % sys.argv[1])
+        except ValueError as e:
+            print(e)
