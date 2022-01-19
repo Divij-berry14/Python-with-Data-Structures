71263888814ef9027f00a2f40bde455ced70ae97
divij14 <divij.berry@zigram.tech>
2022-01-19 20:42:12 +0530

    programs

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -9,6 +9,7 @@
 #             print(j, li)
 #         li[j+1] = temp
 #         print(li)
+
 def InsertionSort(li):
     l = len(li)
     for i in range(1, l):
@@ -17,8 +18,11 @@ def InsertionSort(li):
         while(j>=0 and li[j] > temp):
             li[j+1] = li[j]
             j = j - 1
+            print(li, j)
         li[j+1] = temp
+        print(li)
     print(li)
+
 li = [int(x) for x in input().split()]
 InsertionSort(li)
 print(li)
