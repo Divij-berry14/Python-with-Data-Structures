a358703d5edc83ee7891bda6d12aa95179854874
Divij Berry <divij.berry1999@gmail.com>
2022-02-06 00:45:09 +0530

    linked list delete node

--- Linked List/Delete_ith_Node_LL.py
+++ Linked List/Delete_ith_Node_LL.py
@@ -1,60 +1,62 @@
 class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-def printLL(head):
+
+def print_ll(head):
     while head is not None:
-        print(str(head.data),"-->",end=" ")
-        head=head.next
+        print(str(head.data), "-->", end=" ")
+        head = head.next
     return
 
-def length(head):
-    count=0
+
+def length_ll(head):
+    count = 0
     while head is not None:
-        head=head.next
-        count=count+1
+        head = head.next
+        count = count+1
     return count
 
-def deleteNode(head, i):
-    if i<0 or i>=length(head):
+
+def delete_node(head, i):
+    if i < 0 or i >= length_ll(head):
         return head
-    curr = head 
-    prev=None
-    count=0
-    if  i==0:
+    curr = head
+    prev = None
+    count = 0
+    while count < i:
+        prev = curr
+        curr = curr.next
+        count += 1
+    if i is 0:
         head = curr.next
-        curr = None
         return head
-    while(curr is not None):
-        if count==i: 
-            break 
-        prev = curr 
-        curr = curr.next 
-        count=count+1
-    prev.next = curr.next 
-    #curr=None
+    else:
+        prev.next = curr.next
     return head
 
-def takeInput():
-    inputList=[int(ele) for ele in input().split()]
-    head=None
-    tail=None
-    for currData in inputList:
-        if currData==-1:
+
+def take_input():
+    input_list = [int(i) for i in input().split()]
+    head = None
+    tail = None
+    for currData in input_list:
+        if currData is -1:
             break
-        newNode=Node(currData)
+        new_node = Node(currData)
         if head is None:
-            head=newNode
-            tail=newNode
+            head = new_node
+            tail = new_node
         else:
-            tail.next=newNode
-            tail=newNode
+            tail.next = new_node
+            tail = new_node
             
     return head
 
-head = takeInput()
-printLL(head)
-i=int(input())
-head=deleteNode(head,i)
-printLL(head)
+
+Head = take_input()
+print_ll(Head)
+in_var = int(input())
+Head = delete_node(Head, in_var)
+print_ll(Head)
