ed12a000b36d066f8fd011e4cca5d8d1d5d02f0d
Divij Berry <divij.berry1999@gmail.com>
2022-02-07 23:22:46 +0530

    length of LL recursive

--- Linked List/length_LL_recursively.py
+++ Linked List/length_LL_recursively.py
@@ -1,30 +1,32 @@
+from sys import setrecursionlimit
+
+
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
 
-def lengthRecursive(head):
+
+def length_recursive(head):
     if head is None:
         return 0
-    
-    return 1+lengthRecursive(head.next)
 
-def ll(arr):
-    if len(arr)==0:
+    return 1 + length_recursive(head.next)
+
+
+def ll(ar):
+    if len(ar) is 0:
         return None
-    head = Node(arr[0])
+    head = Node(ar[0])
     last = head
-    for data in arr[1:]:
+    for data in ar[1:]:
         last.next = Node(data)
         last = last.next
     return head
 
-# Main
-from sys import setrecursionlimit
+
 setrecursionlimit(11000)
-# Read the link list elements including -1
-arr=list(int(i) for i in input().strip().split(' '))
-# Create a Linked list after removing -1 from list
-l = ll(arr[:-1])
-len=lengthRecursive(l)
-print(len)
+arr = list(int(i) for i in input().strip().split(' '))
+li = ll(arr[:-1])
+length = length_recursive(li)
+print(length)
