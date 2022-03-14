c54c64076cfa9b4ca3023c219f5c1c339bf4c4e7
Divij Berry <divij.berry1999@gmail.com>
2022-03-14 23:07:23 +0530

    Palindrome

--- Linked List/Palindrome_LL.py
+++ Linked List/Palindrome_LL.py
@@ -1,60 +1,50 @@
 class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-def printLL(head):
-    while head is not None:
-        print(str(head.data),"-->",end=' ')
-        head=head.next
-    print("None")
-    return
 
-def check_palindrome(head) :
+def palindrome_ll(head):
     if head is None:
         return True
-    
-    # Storing all elements of LinkedList into a List s
     s = []
     while head is not None:
         s.append(head.data)
         head = head.next
-    
-    # Now simply check whether elements of 's' are in palindrome or not
-    left = 0
     right = len(s) - 1
+    left = 0
     while left < right:
         if s[left] != s[right]:
             return False
-        left += 1
-        right -= 1
+        left = left + 1
+        right = right - 1
     return True
 
 
+def print_ll(head):
+    while head is not None:
+        print(head.data, "->", end="")
+        head = head.next
+    print("None")
 
-def inputLL():
-    head=None
-    tail=None
-    inputlist=[int(x) for x in input().split()]
-    for currdata in inputlist:
-        if currdata==-1:
+
+def input_ll():
+    li_val = [int(i) for i in input().split()]
+    head = None
+    tail = None
+    for curr_val in li_val:
+        if curr_val is -1:
             break
-        newNode=Node(currdata)
+        new_node = Node(curr_val)
         if head is None:
-            head=newNode
-            tail=head
+            head = new_node
+            tail = new_node
         else:
-            tail.next=newNode
-            tail=newNode
+            tail.next = new_node
+            tail = new_node
     return head
 
 
-head=inputLL()
-printLL(head)
-res=check_palindrome(head)
-print(res)
-
-    
-    
-    
-    
+Head = input_ll()
+print_ll(Head)
+print(palindrome_ll(Head))
