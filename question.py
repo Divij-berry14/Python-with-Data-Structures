c21cc12dfd43ec4849f3d583eb3adfc349d911d3
Divij Berry <divij.berry1999@gmail.com>
2022-03-14 23:16:29 +0530

    code of LL

--- question.py
+++ question.py
@@ -1,143 +1,199 @@
-#
-#
-## Complete the migratoryBirds function below.
-#def migratoryBirds(arr):
-#    d={}
-#    for c in arr:
-#        if( c in d):
-#            d[c]=d[c]+1
-#        else:
-#            d[c]=1
-#    for c1 in d:
-#        if(d[c1]==1):
-#            print(c1)
-#            
-#
-#    
-#
-#
-#arr_count = int(input().strip())
-#
-#arr = list(map(int, input().rstrip().split()))
-#
-#migratoryBirds(arr)
-
-#s1=input()
-#s2=input()
-#s3=[]
-#for i in range(len(s1)):
-#    s3.append(s1[i])
-#    #print(s3)
-#
-##s3.pop()
-#s2=list(s2)
-##print(list(s2))   
-#print(s1)
-#print(s2)
-#print(s3)
-#!/bin/python3
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
 
-# Complete the icecreamParlor function below.
-#def icecreamParlor(m, arr):
-#    arr.sort()
-#    l=len(arr)
-#    j=l-1
-#    for i in range(l):
-#        if(arr[i]+arr[j]==m):
-#            print(i+1,j+1,end="")
-#        else:
-#            j=j-1
-#
-#
-#
-#
-#t = int(input())
-#
-#for t_itr in range(t):
-#    m = int(input())
-#
-#    n = int(input())
-#
-#    arr = list(map(int, input().rstrip().split()))
-
-#    result = icecreamParlor(m, arr)
-
-#test_str = "Geeks"
-#res = [test_str[i: j] for i in range(len(test_str))for j in range(i + 1, len(test_str) + 1)]
-#print(set(test_str))
-#s=input()
-#n=int(input())
-#l=len(s)
-#value=n//l
-#temp=s[:n%l]
-#s=s*value
-#s=s+temp
-##print(temp)
-#print(s)
-#c=s.count("a")
-#print(c)
-#print(new_s)
-
-
-#list(new_s)
-#print(new_s)
-#l1=len(new_s)
-#if(len(new_s)<n):
-#    temp=n-l1
-#    new_s.append(s[:temp])
-#new_s1="".join(map(str, new_s))
-#print(new_s1)
-#c=new_s.count('a')
-#print(value)
-#print(new_s)
-#print(c)
-
-#!/bin/python3
-
-#import math
-#import os
-#import random
-#import re
-#import sys
-
-# Complete the jumpingOnClouds function below.
-def jumpingOnClouds(c):
-    l=len(c)
-    count=0
-    #temp=0
-    i=0
-    while(i<=l-2):
-        print(i)
-        if(c[i]==0 and c[i+2]==0):
-            if(c[i+1]==0):
-                #temp=temp+2
-                i=i+2
-                print("i1",i)
-            count=count+1
-            print(count)
-        if(c[i]==0 and c[i+2]==1):
-            count=count+1
-            i=i+1
-            print("i2",i)
-        print("i3",i)
-    print("i4",i)
-    print(count)
-            
+def length_ll(head):
+    count = 0
+    while head is not None:
+        head = head.next
+        count += 1
     return count
 
 
+def print_i_node_ll(head, i):
+    curr = head
+    count = 0
+    while curr is not None:
+        if count == i:
+            return curr.data
+        curr = curr.next
+        count += 1
 
-if __name__ == '__main__':
-   # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    n = int(input())
+def insert_at_i_pos(head, i, data):
+    if 0 > i > length_ll(head):
+        return head
+    curr = head
+    count = 0
+    prev = None
+    while count < i:
+        prev = curr
+        curr = curr.next
+        count += 1
+    new_node = Node(data)
+    if prev is None:
+        head = new_node
+    else:
+        prev.next = new_node
+    new_node.next = curr
+    return head
 
-    c = list(map(int, input().rstrip().split()))
 
-    result = jumpingOnClouds(c)
-    print("value",result)
+def delete_node(head, i):
+    if i < 0 or i >= length_ll(head):
+        return head
+    curr = head
+    prev = None
+    count = 0
+    while count < i:
+        prev = curr
+        curr = curr.next
+        count += 1
+    if i is 0:
+        head = curr.next
+        return head
+    else:
+        prev.next = curr.next
+    return head
 
-    #fptr.write(str(result) + '\n')
 
-    #fptr.close()
+def length_recursive(head):
+    if head is None:
+        return 0
+    return 1 + length_recursive(head.next)
+
+
+def insert_at_pos_rec(head, i, data):
+    if i < 0:
+        return head
+
+    if i == 0:
+        new_node = Node(data)
+        new_node.next = head
+        return new_node
+
+    if head is None:
+        return None
+
+    small_head = insert_at_pos_rec(head.next, i-1, data)
+    head.next = small_head
+    return head
+
+
+def delete_node_rec(head, i):
+    if i < 0:
+        return head
+
+    if i is 0:
+        return head.next
+
+    small_head = delete_node_rec(head.next, i-1)
+    head.next = small_head
+    return head
+
+
+def append_lst_n_to_first(head, n):
+    count = length_ll(head)
+    curr = head
+    prev = None
+    temp1 = head
+    while curr is not None:
+        curr = curr.next
+    temp2 = count - n
+    temp3 = 0
+    while temp1 is not None and temp3 != temp2:
+        prev = temp1
+        temp1 = temp1.next
+        temp3 += 1
+    prev.next = None
+    head1 = temp1
+    while temp1.next is not None:
+        temp1 = temp1.next
+    temp1.next = head
+    return head1
+
+
+def eliminate_duplicate(head):
+    curr = head
+    while curr.next is not None:
+        if curr.data == curr.next.data:
+            curr.next = curr.next.next
+        else:
+            curr = curr.next
+    return head
+
+
+def reverse_ll(head):
+    curr = head
+    prev = None
+    while curr is not None:
+        temp1 = curr.next
+        curr.next = prev
+        prev = curr
+        curr = temp1
+    return prev
+
+
+def palindrome_ll(head):
+    if head is None:
+        return True
+    s = []
+    while head is not None:
+        s.append(head.data)
+        head = head.next
+    right = len(s) - 1
+    left = 0
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left = left + 1
+        right = right - 1
+    return True
+
+
+def print_ll(head):
+    while head is not None:
+        print(head.data, "->", end="")
+        head = head.next
+    print("None")
+
+
+def input_ll():
+    li_val = [int(i) for i in input().split()]
+    head = None
+    tail = None
+    for curr_val in li_val:
+        if curr_val is -1:
+            break
+        new_node = Node(curr_val)
+        if head is None:
+            head = new_node
+            tail = new_node
+        else:
+            tail.next = new_node
+            tail = new_node
+    return head
+
+
+Head = input_ll()
+print_ll(Head)
+# print(length_ll(Head))
+# print(print_i_node_ll(Head, 3))
+# Head = insert_at_i_pos(Head, 2, 100)
+# print_ll(Head)
+# Head = delete_node(Head, 3)
+# print_ll(Head)
+print(length_recursive(Head))
+# Head = insert_at_pos_rec(Head, 1, 70)
+# print_ll(Head)
+# Head = delete_node_rec(Head, 2)
+# print_ll(Head)
+# Head = append_lst_n_to_first(Head, 5)
+# print_ll(Head)
+# Head = eliminate_duplicate(Head)
+# print_ll(Head)
+# Head = reverse_ll(Head)
+# print_ll(Head)
+print(palindrome_ll(Head))
