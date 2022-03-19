190bdd4b77587898184dc21695c2a5a1eef6f2a5
Divij Berry <divij.berry1999@gmail.com>
2022-03-20 02:07:09 +0530

    Merge_Sort_LL.py

--- Linked List/Merge_Two_Sorted_LL.py
+++ Linked List/Merge_Two_Sorted_LL.py
@@ -1,66 +1,67 @@
 class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-def print_LL(head):
-    while(head is not None):
-        print(str(head.data),"-->",end=' ')
-        head=head.next
-    print("None")
-    return head
 
-def merge_two_sorted_arrays(head1,head2):
-    finalHead=None
-    finalTail=None
+def merge_sorted_ll(head1, head2):
+    if head1 is None:
+        return head2
+    if head2 is None:
+        return head1
+
     if head1.data > head2.data:
-            finalHead=head2
-            finalTail=head2
-            head2=head2.next
+        final_head = head2
+        final_tail = head2
+        head2 = head2.next
+    else:
+        final_head = head1
+        final_tail = head1
+        head1 = head1.next
+
     while head1 is not None and head2 is not None:
         if head1.data > head2.data:
-            finalTail.next=head2
-            finalTail=finalTail.next
-            head2=head2.next
-        
+            final_tail.next = head2
+            final_tail = final_tail.next
+            head2 = head2.next
         else:
-            finalTail.next=head1
-            finalTail=finalTail.next
-            head1=head1.next
-    
+            final_tail.next = head1
+            final_tail = final_tail.next
+            head1 = head1.next
     if head1 is not None:
-        finalTail.next=head1
+        final_tail.next = head1
     if head2 is not None:
-        finalTail.next=head2
-    
-    return finalHead
+        final_tail.next = head2
+    return final_head
 
-def input_LL():
-    head=None
-    tail=None
-    input_list=[int(x) for x in input().split()]
-    for currdata in input_list:
-        if currdata==-1:
+
+def print_ll(head):
+    while head is not None:
+        print(head.data, "->", end="")
+        head = head.next
+    print("None")
+
+
+def input_ll():
+    li_val = [int(i) for i in input().split()]
+    head = None
+    tail = None
+    for curr_val in li_val:
+        if curr_val is -1:
             break
-        newNode=Node(currdata)
+        new_node = Node(curr_val)
         if head is None:
-            head=newNode
-            tail=head
+            head = new_node
+            tail = new_node
         else:
-            tail.next=newNode
-            tail=newNode
+            tail.next = new_node
+            tail = new_node
     return head
 
-head1=input_LL()
-head2=input_LL()
-head=merge_two_sorted_arrays(head1,head2)
-print_LL(head)
 
-    
-        
-    
-            
-            
-            
-        
-            
+Head1 = input_ll()
+Head2 = input_ll()
+print_ll(Head1)
+print_ll(Head2)
+Head = merge_sorted_ll(Head1, Head2)
+print_ll(Head)
