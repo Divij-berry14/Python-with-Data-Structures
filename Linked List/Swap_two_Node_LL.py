df2134bb6ccc69d2e227c9e6a5ea361aa66b551b
Divij Berry <divij.berry1999@gmail.com>
2022-03-25 23:30:50 +0530

    Swap_two_Node_LL.py

--- Linked List/Swap_two_Node_LL.py
+++ Linked List/Swap_two_Node_LL.py
@@ -3,6 +3,7 @@ class Node:
         self.data = data
         self.next = None
 
+
 def swap_nodes(head,i,j):
         check=head
         count=0
@@ -63,14 +64,14 @@ def ll(arr):
         last = last.next
     return head
 
+
 def printll(head):
     while head:
         print(head.data, end=' ')
         head = head.next
     print()
 
-# Main
-# Read the link list elements including -1
+
 arr=list(int(i) for i in input().strip().split(' '))
 # Create a Linked list after removing -1 from list
 l = ll(arr[:-1])
