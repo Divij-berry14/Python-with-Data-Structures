961320d2c7b46316a3d77bf3b32f5380d74e15fe
Divij Berry <divij.berry1999@gmail.com>
2022-03-26 23:53:08 +0530

    Swap_two_Node_LL.py

--- Linked List/Swap_two_Node_LL.py
+++ Linked List/Swap_two_Node_LL.py
@@ -4,77 +4,79 @@ class Node:
         self.next = None
 
 
-def swap_nodes(head,i,j):
-        check=head
-        count=0
-        while check:
-            if count==i:
-                x=check.data
-            elif count==j:
-                y=check.data
-            count=count+1
-            check=check.next
- 
-        if x == y: 
-            return 
-  
-        
-        prevX = None
-        currX = head 
-        while currX != None and currX.data != x: 
-            prevX = currX 
-            currX = currX.next
-  
-        
-        prevY = None
-        currY = head 
-        while currY != None and currY.data != y: 
-            prevY = currY 
-            currY = currY.next
-  
-        
-        if currX == None or currY == None: 
-            return 
-       
-        if prevX != None: 
-            prevX.next = currY 
-        else: 
-            head = currY 
-  
-       
-        if prevY != None: 
-            prevY.next = currX 
-        else: 
-            head = currX 
-  
-        
-        temp = currX.next
-        currX.next = currY.next
-        currY.next = temp 
+def swap_nodes_ll(head, i, j):
+    curr = head
+    count = 0
+    x = 0
+    y = 0
+    if i == j:
         return head
-            
+    while curr is not None:
+        if count == i:
+            x = curr.data
+        elif count == j:
+            y = curr.data
+        curr = curr.next
+        count += 1
+    if x == y:
+        return head
 
-def ll(arr):
-    if len(arr)==0:
-        return None
-    head = Node(arr[0])
-    last = head
-    for data in arr[1:]:
-        last.next = Node(data)
-        last = last.next
+    prev_x = None
+    curr_x = head
+    while curr_x is not None and curr_x.data != x:
+        prev_x = curr_x
+        curr_x = curr_x.next
+
+    prev_y = None
+    curr_y = head
+    while curr_y is not None and curr_y.data != y:
+        prev_y = curr_y
+        curr_y = curr_y.next
+
+    if curr_x is None or curr_y is None:
+        return head
+
+    if prev_x is not None:
+        prev_x.next = curr_y
+    else:
+        head = curr_y
+
+    if prev_y is not None:
+        prev_y.next = curr_x
+    else:
+        head = curr_x
+
+    temp = curr_x.next
+    curr_x.next = curr_y.next
+    curr_y.next = temp
     return head
 
 
-def printll(head):
-    while head:
-        print(head.data, end=' ')
+def print_ll(head):
+    while head is not None:
+        print(head.data, "->", end="")
         head = head.next
-    print()
+    print("None")
 
 
-arr=list(int(i) for i in input().strip().split(' '))
-# Create a Linked list after removing -1 from list
-l = ll(arr[:-1])
-i, j=list(int(i) for i in input().strip().split(' '))
-l = swap_nodes(l, i, j)
-printll(l)
+def input_ll():
+    li_val = [int(i) for i in input().split()]
+    head = None
+    tail = None
+    for curr_val in li_val:
+        if curr_val is -1:
+            break
+        new_node = Node(curr_val)
+        if head is None:
+            head = new_node
+            tail = new_node
+        else:
+            tail.next = new_node
+            tail = new_node
+    return head
+
+
+Head = input_ll()
+print_ll(Head)
+Head = swap_nodes_ll(Head, 3, 4)
+print_ll(Head)
