dd2c2ec0d898211104aca8fd229af92f1f91762d
Divij Berry <divij.berry1999@gmail.com>
2022-04-13 00:33:16 +0530

    Balanced_Paranthesis.py

--- Stack/Balanced_Paranthesis.py
+++ Stack/Balanced_Paranthesis.py
@@ -1,27 +1,27 @@
-def is_balanced(string):
-    s=[]
-    for char in string:
+def is_balanced(st):
+    s = []
+    for char in st:
         if char in '({[':
             s.append(char)
         elif char is ')':
-            if (not s or s[-1]!='('):
+            if not s or s[-1] != '(':
                 return False
             s.pop()
         elif char is '}':
-            if (not s or s[-1]!='{'):
+            if not s or s[-1] != '{':
                 return False
             s.pop()
         elif char is ']':
-            if (not s or s[-1]!='['):
+            if not s or s[-1] != '[':
                 return False
             s.pop()
     if not s:
         return True
     return False
 
-string=input()
-ans=is_balanced(string)
 
+string = input()
+ans = is_balanced(string)
 if ans:
     print("true")
 else:
