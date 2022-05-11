40adf32dd9a8c105ea1e78aea69c7f6568e3b21f
Divij Berry <divij.berry1999@gmail.com>
2022-05-11 23:51:17 +0530

    .\Queue_using_2_stacks.py

--- Queue/Queue_using_2_stacks.py
+++ Queue/Queue_using_2_stacks.py
@@ -1,46 +1,46 @@
-class Queue_using_2_stacks():
+class QueueUsing2stacks:
     def __init__(self):
         self.__s1 = []
         self.__s2 = []
     
-    def Enqueue(self, data):
+    def enqueue(self, data):
         #O(n)
-        while (len(self.__s1) != 0):
+        while len(self.__s1) != 0:
             self.__s2.append(self.__s1.pop())
         
         self.__s1.append(data)
         
-        while(len(self.__s2) != 0):
+        while len(self.__s2) != 0:
             self.__s1.append(self.__s2.pop())
         return
     
-    def Dequeue(self):
+    def dequeue(self):
         #O(1)
-        if(len(self.__s1) == 0):
+        if len(self.__s1) == 0:
             return -1
         return self.__s1.pop()
     
-    def Qfront(self):
+    def q_front(self):
         if len(self.__s1) == 0:
             return -1
         return self.__s1[-1]
     
-    def Qsize(self):
+    def q_size(self):
         return len(self.__s1)
     
-    def isEmpty(self):
-        return self.Qsize() == 0
+    def is_empty(self):
+        return self.q_size() == 0
     
     
-q = Queue_using_2_stacks()
-q.Enqueue(34)
-q.Enqueue(45)
-q.Enqueue(23)
-q.Enqueue(12)
-print("Front element of the queue is:", q.Qfront())
-print("If the elememt is empty of full", q.isEmpty())
-print("Size of the queue:", q.Qsize())
+q = QueueUsing2stacks()
+q.enqueue(34)
+q.enqueue(45)
+q.enqueue(23)
+q.enqueue(12)
+print("Front element of the queue is:", q.q_front())
+print("If the elememt is empty of full", q.is_empty())
+print("Size of the queue:", q.q_size())
 print("Elements of the queue:")
-while(q.isEmpty() is False):
-    print(q.Qfront())
-    q.Dequeue()
+while q.is_empty() is False:
+    print(q.q_front())
+    q.dequeue()
