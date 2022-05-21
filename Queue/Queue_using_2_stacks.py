d599c661f7032df62cfef5541d6bceae5e69e3ac
Divij Berry <divij.berry1999@gmail.com>
2022-05-21 23:41:26 +0530

    Queue_using_2_stacks.py

--- Queue/Queue_using_2_stacks.py
+++ Queue/Queue_using_2_stacks.py
@@ -2,36 +2,36 @@ class QueueUsing2stacks:
     def __init__(self):
         self.__s1 = []
         self.__s2 = []
-    
+
     def enqueue(self, data):
-        #O(n)
+        # O(n)
         while len(self.__s1) != 0:
             self.__s2.append(self.__s1.pop())
-        
+
         self.__s1.append(data)
-        
+
         while len(self.__s2) != 0:
             self.__s1.append(self.__s2.pop())
         return
-    
+
     def dequeue(self):
-        #O(1)
+        # O(1)
         if len(self.__s1) == 0:
             return -1
         return self.__s1.pop()
-    
+
     def q_front(self):
         if len(self.__s1) == 0:
             return -1
         return self.__s1[-1]
-    
+
     def q_size(self):
         return len(self.__s1)
-    
+
     def is_empty(self):
         return self.q_size() == 0
-    
-    
+
+
 q = QueueUsing2stacks()
 q.enqueue(34)
 q.enqueue(45)
